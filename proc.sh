8985aea8cdf37aa512efca634e4500f9f0257569
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-09-27 13:48:57 -0500

    proc

--- proc.sh
+++ proc.sh
@@ -13,3 +13,7 @@ flirt \
     -out OUTPUTS/rmprage3
 
 fslmaths INPUTS/mprage1 -add OUTPUTS/rmprage2 -add OUTPUTS/rmprage3 -div 3 OUTPUTS/mrmprage
+
+flirt -usesqform -cost mutualinfo -in INPUTS/pd -ref OUTPUTS/mrmprage -out OUTPUTS/rpd
+
+fslmaths OUTPUTS/rpd -s 10 OUTPUTS/srpd
