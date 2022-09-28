2be593e26ae4d8a830fa1f4d0e91669edcaa07e2
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-09-28 13:06:33 -0500

    testing

--- testrun.sh
+++ testrun.sh
@@ -4,8 +4,20 @@ docker run \
     --mount type=bind,src=`pwd -P`/freesurfer_license.txt,dst=/usr/local/freesurfer/license.txt \
     --mount type=bind,src=`pwd -P`/INPUTS,dst=/INPUTS \
     --mount type=bind,src=`pwd -P`/OUTPUTS,dst=/OUTPUTS \
-    --mount type=bind,src=`pwd -P`/OUTPUTS2,dst=/OUTPUTS2 \
     -e DISPLAY=host.docker.internal:0 \
     baxterprogers/freesurfer720:v2.0.0 \
     --t1_niigz /OUTPUTS/smrmprage.nii.gz \
     --out_dir /OUTPUTS2
+
+
+
+docker run -it \
+    --mount type=bind,src=`pwd -P`/freesurfer_license.txt,dst=/usr/local/freesurfer/license.txt \
+    --mount type=bind,src=`pwd -P`/INPUTS,dst=/INPUTS \
+    --mount type=bind,src=`pwd -P`/OUTPUTS,dst=/OUTPUTS \
+    -e DISPLAY=host.docker.internal:0 \
+    --entrypoint bash \
+    baxterprogers/freesurfer720:v2.0.0
+
+#    recon-all -watershed atlas -autorecon1 -sd /OUTPUTS -s MRMPRAGE -i /OUTPUTS/mrmprage.nii.gz
+#    recon-all -watershed atlas -autorecon1 -sd /OUTPUTS -s SMRMPRAGE -i /OUTPUTS/smrmprage.nii.gz
