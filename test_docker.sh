1af9995f49e0004c0c0fc8568bbe0596ed4cf354
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-11-08 11:41:26 -0600

    testcode

--- test_docker.sh
+++ test_docker.sh
@@ -5,6 +5,15 @@ docker run \
     --mount type=bind,src=`pwd -P`/OUTPUTS,dst=/OUTPUTS \
     prept1:test \
     --pd_niigz /INPUTS/pd.nii.gz \
+    --t1_niigzs /INPUTS/mprage1.nii.gz \
+    --fwhm 10 \
+    --out_dir /OUTPUTS
+
+docker run \
+    --mount type=bind,src=`pwd -P`/INPUTS,dst=/INPUTS \
+    --mount type=bind,src=`pwd -P`/OUTPUTS,dst=/OUTPUTS \
+    prept1:test \
+    --pd_niigz /INPUTS/pd.nii.gz \
     --t1_niigzs /INPUTS/mprage1.nii.gz /INPUTS/mprage2.nii.gz /INPUTS/mprage3.nii.gz \
     --fwhm 10 \
     --out_dir /OUTPUTS
